6d7e520af3b979cd0b5691cdb36ab150e38cd835
Nick Teets <nickteets@gmail.com>
2017-09-06 17:58:06 -0600

    Implement suggest and write functionality

--- scripts/Node.js
+++ scripts/Node.js
@@ -2,13 +2,9 @@ class Node {
 
   constructor(data) {
     this.data = data;
-    this.children = [];
+    this.children = {};
     this.isWord = false;
   }
-
-  // add() {
-  //   this.children = new Node(letter)
-  // }
 
 }
 
--- tests/Trie-test.js
+++ tests/Trie-test.js
@@ -8,30 +8,43 @@ describe('Trie test', () => {
   })
 
   it('should be able to count the inserted words', () => {
-    let completion = new Trie;
+    let completion = new Trie();
+
+    expect(completion.count).to.eq(0);
 
     assert.equal(completion.count, 0)
 
     completion.insert('pizza');
-    assert.equal(completion.count, 1);
+    expect(completion.count).to.eq(1);
 
     completion.insert('apple');
-    assert.equal(completion.count, 2)
+    completion.insert('zip');
+
+    expect(completion.count).to.eq(3);
 
   })
 
-  it('should be able to return words based on common prefix', () => {
-    let completion = new Trie;
+  it('should should be able to offer suggestions based on a prefix', () => {
+    let completion = new Trie();
 
-    assert.equal(completion.count, 0)
+    expect(completion.count).to.eq(0);
+
     completion.insert('pizza');
     completion.insert('pizzeria');
     completion.insert('apple');
-    assert.equal(completion.count, 3)
+    completion.insert('arson');
+    completion.insert('bumble');
+    completion.insert('friend');
+    completion.insert('hometeam');
+    completion.insert('pitchfork');
+    completion.insert('altruism');
+    completion.insert('pasta');
 
-    assert.deepEqual(completion.suggest('piz'), ['pizza', 'pizzeria'])
-
-    assert.deepEqual(completion.suggest('ap'), ['apple'])
+    expect(completion.count).to.eq(10);
+    expect(completion.suggest('a')).to.deep.eq(['apple', 'arson', 'altruism']);
+    expect(completion.suggest('p')).to.deep.eq(['pizza', 'pizzeria', 'pitchfork', 'pasta']);
+    expect(completion.suggest('pi')).to.deep.eq(['pizza', 'pizzeria', 'pitchfork']);
+    expect(completion.suggest('piz')).to.deep.eq(['pizza', 'pizzeria']);
   })
 
 })
