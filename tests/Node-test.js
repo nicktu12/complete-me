175df2af593741ed2ce0d640d2b30ac9bcc4e95f
Nick Teets <nickteets@gmail.com>
2017-09-07 09:47:35 -0600

    Implement tests over node

--- tests/Node-test.js
+++ tests/Node-test.js
@@ -1,5 +1,31 @@
 import { expect } from 'chai';
 import Node from '../scripts/Node'
 
-describe('Example Test File', () => {
+describe('NODE TEST', () => {
+
+  it('should be an object', () => {
+    let node = new Node();
+
+    expect(typeof(node)).to.eq('object');
+  })
+
+  it('should have four properties', () => {
+    let node = new Node();
+
+    expect(Object.keys(node).length).to.eq(4);
+    expect(Object.keys(node)).to.deep.eq(['data', 'children', 'isWord', 'selectProp']);
+  })
+
+  it('should not be a complete word by default', () => {
+    let node = new Node();
+
+    expect(node.isWord).to.eq(false);
+  })
+
+  it('should not have children by default', () => {
+    let node = new Node();
+
+    expect(Object.keys(node.children).length).to.eq(0);
+  })
+
 })
